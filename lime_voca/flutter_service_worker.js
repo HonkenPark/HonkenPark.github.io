d87a2f94b7f61b30092d2fd9c6b9d5d6abe00288
Honken Park <honken.park@gmail.com>
2025-09-20 01:51:36 +0900

    mobile keyboard

--- lime_voca/flutter_service_worker.js
+++ lime_voca/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "42224545a4f13d63d42a733f6aa01609",
+const RESOURCES = {"flutter_bootstrap.js": "e378a5896f683c3d0050b743277db135",
 "version.json": "547adef19d293ba454360dd16b038fd4",
 "index.html": "2c23ade7769ba37f9f44d9277c9cc370",
 "/": "2c23ade7769ba37f9f44d9277c9cc370",
-"main.dart.js": "2a9beefa01516f4f9b06f1fba7636f79",
+"main.dart.js": "44a574915b08c72266bd9d7a89de606c",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "9d640cebcd0bc093440a226b1fdae549",
 "icons/Icon-192.png": "123b9c2e457b1ed228e1bd0a623f8bf0",
